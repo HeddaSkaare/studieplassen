603179de260608775e8c4ae623cb8c9e455514f8
Lavrans Blytt <lavransb@stud.ntnu.no>
2023-02-13 13:49:59 +0100

    Added a map and navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "leaflet/dist/leaflet.css";
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import { MapContainer, TileLayer } from "react-leaflet";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <div>
+                <Navbar bg="primary" variant="dark">
+                    <Container>
+                        <Navbar.Brand href="#">Navbar</Navbar.Brand>
+                    </Container>
+                </Navbar>
+            </div>
+            <div>
+                <MapContainer
+                    center={[63.43, 10.39]}
+                    zoom={14}
+                    scrollWheelZoom={false}
+                    preferCanvas={true}
+                >
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                </MapContainer>
+            </div>
+        </div>
+    );
 }
 
 export default App;
