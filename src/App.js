bac2cbe2eb4ae258d3a48977856987c1e1bcdf43
Hedda Skaare <heddsk@stud.ntnu.no>
2023-02-23 14:32:14 +0100

    sidebar

--- src/App.js
+++ src/App.js
@@ -9,11 +9,15 @@ import './App.css';
 import React from 'react';
 import Markers from './components/markers';
 import Map from './components/map';
+import SideBar from "./components/sideBar";
 
 
 function App() {  
   return (
+    <div id='app'>
+    <SideBar></SideBar>
     <Map></Map>
+    </div>
   );
 }
 export default App;
