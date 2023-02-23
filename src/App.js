4ce503863d4afc934346fa168753af8710fd61cc
Rebekka Øvensen Aanderaa <Rebekka@dhcp-10-22-16-160.wlan.ntnu.no>
2023-02-23 15:55:59 +0100

    added navbar

--- src/App.js
+++ src/App.js
@@ -6,17 +6,21 @@ import {
   Marker,
 } from 'react-leaflet';
 import './App.css';
+import './css/map.css';
 import React from 'react';
 import Markers from './components/markers';
 import Map from './components/map';
 import SideBar from "./components/sideBar";
+import NavBar from "./components/navBar";
 
 
 function App() {  
   return (
     <div id='app'>
+    <NavBar></NavBar>
     <SideBar></SideBar>
     <Map></Map>
+    
     </div>
   );
 }
