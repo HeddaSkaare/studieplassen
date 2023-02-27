465d2b14adade3adf813d303b994a6241abe1255
Lavrans Blytt <lavransb@stud.ntnu.no>
2023-02-27 11:52:10 +0100

    change to sliders in "legg til info"

--- src/components/addPlace.jsx
+++ src/components/addPlace.jsx
@@ -3,8 +3,8 @@ import { Button, Form } from "react-bootstrap";
 import TimePicker from "react-bootstrap-time-picker";
 import NavBar from "./navBar";
 export default function AddPlace() {
-    const [noice, setNoice] = useState("");
-    const [rating, setRating] = useState("")
+    const [noice, setNoice] = useState(3);
+    const [rating, setRating] = useState(3)
     const [from, setFrom] = useState("");
     const [to, setTo] = useState("")
     function validateForm() {
@@ -12,6 +12,14 @@ export default function AddPlace() {
     }
     function handleSubmit(event) {
       event.preventDefault();
+    }
+    function handleNoiceChange(event) {
+      event.preventDefault();
+      setNoice(event.target.value)
+    }
+    function handleRatingChange(event) {
+      event.preventDefault();
+      setRating(event.target.value)
     }
     return (
         <div>
@@ -22,24 +30,14 @@ export default function AddPlace() {
           <Form.Group className="email" size="lg" controlId="email">
             <Form.Label className="email">Støy</Form.Label>
             <br></br>
-            <Form.Control
-              autoFocus
-              className="Text"
-              type="text"
-              value={noice}
-              onChange={(e) => setNoice(e.target.value)}
-              />
+            <input type="range" min={1} max={5} value={noice} onChange={handleNoiceChange} />
+      <p>Value: {noice}</p>
           </Form.Group>
           <Form.Group className="email" size="lg" controlId="email">
             <Form.Label className="email">Vurdering</Form.Label>
             <br></br>
-            <Form.Control
-              autoFocus
-              className="Text"
-              type="text"
-              value={rating}
-              onChange={(e) => setRating(e.target.value)}
-              />
+            <input type="range" min={1} max={5} value={rating} onChange={handleRatingChange} />
+      <p>Value: {rating}</p>
           </Form.Group>
           <Form.Label className="email"> Tilgjengelighet</Form.Label>
           <br />
