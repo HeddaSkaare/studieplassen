7d7571e3aa167e58b509d025ddbe8d562049f2c8
Hedda Skaare <heddsk@stud.ntnu.no>
2023-04-25 14:42:56 +0200

    fil

---
+++ backend/Todata.py
@@ -0,0 +1,44 @@
+from flask import Flask, request, jsonify
+import psycopg2
+
+app = Flask(__name__)
+
+# Database configuration
+db_config = {
+    'host': 'your_host',
+    'port': 'your_port',
+    'dbname': 'your_dbname',
+    'user': 'your_username',
+    'password': 'your_password'
+}
+
+# Connect to the database
+conn = psycopg2.connect(**db_config)
+
+# Create a cursor object
+cur = conn.cursor()
+
+# Create a table to store JSON data
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS json_data (
+        id SERIAL PRIMARY KEY,
+        data JSONB
+    )
+""")
+conn.commit()
+
+# Define a route to handle incoming JSON data
+@app.route('/json-data', methods=['POST'])
+def store_json_data():
+    data = request.json
+    
+    # Insert the JSON data into the database
+    cur.execute("""
+        INSERT INTO json_data (data) VALUES (%s)
+    """, (json.dumps(data),))
+    conn.commit()
+    
+    return jsonify({'message': 'Data stored successfully'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
