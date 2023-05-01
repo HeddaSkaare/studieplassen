337e730a56e142619b623edf8cf55f580b4ac306
Lavrans Blytt <lavransb@stud.ntnu.no>
2023-05-01 12:00:16 +0200

    Start fuzzygis

--- src/components/addPlace.jsx
+++ src/components/addPlace.jsx
@@ -4,52 +4,81 @@ import TimePicker from "react-bootstrap-time-picker";
 import NavBar from "./navBar";
 export default function AddPlace() {
     const [noice, setNoice] = useState(3);
-    const [rating, setRating] = useState(3)
+    const [rating, setRating] = useState(3);
     const [from, setFrom] = useState("");
-    const [to, setTo] = useState("")
-    
+    const [to, setTo] = useState("");
+
     function validateForm() {
-      return noice.length > 0 && rating <= 10 && rating>=1;
+        return noice.length > 0 && rating <= 10 && rating >= 1;
     }
     function handleSubmit(event) {
-      event.preventDefault();
+        event.preventDefault();
     }
     function handleNoiceChange(event) {
-      event.preventDefault();
-      setNoice(event.target.value)
+        event.preventDefault();
+        setNoice(event.target.value);
     }
     function handleRatingChange(event) {
-      event.preventDefault();
-      setRating(event.target.value)
+        event.preventDefault();
+        setRating(event.target.value);
     }
     return (
         <div>
             <NavBar />
-      <div>
-      <header id='header'><h1>Legg Til Info</h1></header>
-    <Form className="Login" onSubmit={handleSubmit}>
-          <Form.Group className="email" size="lg" controlId="email">
-            <Form.Label className="email">Støy</Form.Label>
-            <br></br>
-            <input type="range" min={1} max={5} value={noice} onChange={handleNoiceChange} />
-      <p>Value: {noice}</p>
-          </Form.Group>
-          <Form.Group className="email" size="lg" controlId="email">
-            <Form.Label className="email">Vurdering</Form.Label>
-            <br></br>
-            <input type="range" min={1} max={5} value={rating} onChange={handleRatingChange} />
-      <p>Value: {rating}</p>
-          </Form.Group>
-          <Form.Label className="email"> Tilgjengelighet</Form.Label>
-          <br />
-          <TimePicker className="email" step={60} value={from} onChange={(e) => setFrom(e)} />
-          <TimePicker className="email" step={60} value={to} onChange={(e) => setTo(e)} />
-          <br />
-          <Button block size="lg" type="submit" disabled={!validateForm()}>
-            Legg Til
-          </Button>
-        </Form>
-              </div>
-              </div>
+            <div>
+                <header id="header">
+                    <h1>Legg Til Info</h1>
+                </header>
+                <Form className="Login" onSubmit={handleSubmit}>
+                    <Form.Group className="email" size="lg" controlId="email">
+                        <Form.Label className="email">Støy</Form.Label>
+                        <br></br>
+                        <input
+                            type="range"
+                            min={1}
+                            max={5}
+                            value={noice}
+                            onChange={handleNoiceChange}
+                        />
+                        <p>Value: {noice}</p>
+                    </Form.Group>
+                    <Form.Group className="email" size="lg" controlId="email">
+                        <Form.Label className="email">Vurdering</Form.Label>
+                        <br></br>
+                        <input
+                            type="range"
+                            min={1}
+                            max={5}
+                            value={rating}
+                            onChange={handleRatingChange}
+                        />
+                        <p>Value: {rating}</p>
+                    </Form.Group>
+                    <Form.Label className="email"> Tilgjengelighet</Form.Label>
+                    <br />
+                    <TimePicker
+                        className="email"
+                        step={60}
+                        value={from}
+                        onChange={(e) => setFrom(e)}
+                    />
+                    <TimePicker
+                        className="email"
+                        step={60}
+                        value={to}
+                        onChange={(e) => setTo(e)}
+                    />
+                    <br />
+                    <Button
+                        block
+                        size="lg"
+                        type="submit"
+                        disabled={!validateForm()}
+                    >
+                        Legg Til
+                    </Button>
+                </Form>
+            </div>
+        </div>
     );
-  }
+}
--- src/components/filterSite.jsx
+++ src/components/filterSite.jsx
@@ -1,154 +1,270 @@
 import React, { useState } from "react";
-import { Button, htmlForm } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 import "../css/filterSite.css";
+import data from "../data/vurdering.json";
 import NavBar from "./navBar";
-import { useNavigate } from 'react-router-dom';
-
-function RadioButtons(){
-    return(
-        <div>
-            <input type="radio" id="1" value="1">1</input>
-            <input type="radio" id="1" value="2">2</input>
-            <input type="radio" id="1" value="3">3</input>
-            <input type="radio" id="1" value="4">4</input>
-            <input type="radio" id="1" value="5">5</input>
-        </div>
-    )
-}
 
 export default function FilterSite() {
-//   const [stoy,setStoy] = useState();
-//   const [tilgjengelighet,setTilgjengelighet] = useState();
-//   const [vurdering,setVurdering] = useState();
-//   const [nerhet,setNerhet] = useState();
- 
-//   const btn = document.querySelector('#sok');
-//   const radioButtons = document.querySelectorAll('input[name="styles"]');
-//   btn.addEventListener("click", ()=>{
-//     let selectedStoy;
-//     htmlFor (const radioButton of radioButtons){
-//         if (radioButton.checked){
-//             selectedStoy = radioButton.value;
-//             break;
-//         }
-//         console.log(selectedStoy)
-//     }
-    
-//   })
-  const radioButtons1 = document.querySelectorAll('input[name="styles"]');
-  const radioButtons2 = document.querySelectorAll('input[name="styles1"]');
-  const radioButtons3 = document.querySelectorAll('input[name="styles2"]');
-  const radioButtons4 = document.querySelectorAll('input[name="styles3"]');
-  const navigate = useNavigate();
+    const [stoy, setStoy] = useState(3);
+    const [checkStoy, setCheckStoy] = useState(false);
+    const [vurdering, setVurdering] = useState(3);
+    const [checkVurdering, setCheckVurdering] = useState(false);
+    const [nerhet, setNerhet] = useState(3);
+    const [checkNerhet, setCheckNerhet] = useState(false);
+    const navigate = useNavigate();
 
-  function handleSubmit(event) {
-    let selectedStoy;
-    let selectedTilgjengelighet;
-    let selectedVurdering;
-    let selectedNerhet;
-    
-    for (const radioButton1 of radioButtons1){
-        if (radioButton1.checked){
-            selectedStoy = radioButton1.value;
-            break;
-        }
+    function onChangeStoy(event) {
+        setStoy(event.target.value);
     }
-    for (const radioButton2 of radioButtons2){
-        if (radioButton2.checked){
-            selectedTilgjengelighet = radioButton2.value;
-            break;
-        }
-    }
-    for (const radioButton3 of radioButtons3){
-        if (radioButton3.checked){
-            selectedVurdering = radioButton3.value;
-            break;
-        }
-    }
-    for (const radioButton4 of radioButtons4){
-        if (radioButton4.checked){
-            selectedNerhet = radioButton4.value;
-            break;
-        }
-    }
-    console.log(selectedStoy)
-    console.log(selectedTilgjengelighet)
-    console.log(selectedVurdering)
-    console.log(selectedNerhet)
-    navigate("/")
-  }
-  return (
-    <>
-    <div>
-        <NavBar></NavBar>
-    </div>
-    <header class='header'><h1>Finn studieplass</h1></header>
-     <p>Her kan du legge inn dine ønsker for studieplassen</p>
-    <div id='filterBox'>
-        
-        <div id='element'>
-            <div class='headerWeights'>
-                 <div>
-                    <h2>Støy</h2>
-                </div> 
-            <div id= 'weight'>
-                <input type="checkbox" name="check"></input> 
-                <label htmlFor="box-shadow"> Viktig? </label>
-            </div>
 
-            </div>
-            <div id='stoy'>
-                <input type="radio" name="styles" id="box-shadow" className="custom-radio" value={1}></input><label htmlFor="box-shadow">1</label>
-                <input type="radio" name="styles" id="box-shadow" className="custom-radio" value={2}></input><label htmlFor="box-shadow">2</label>
-                <input type="radio" name="styles" id="box-shadow" className="custom-radio" value={3}></input><label htmlFor="box-shadow">3</label>
-                <input type="radio" name="styles" id="box-shadow" className="custom-radio" value={4}></input><label htmlFor="box-shadow">4</label>
-                <input type="radio" name="styles" id="box-shadow" className="custom-radio" value={5}></input><label htmlFor="box-shadow">5</label>
-            </div>
-        </div>
-        <div id='element'>
-            <div class='headerWeights'>
-                 <div>
-                    <h2>Tilgjengelighet</h2>
-                </div> 
-            <div id= 'weight'>
-                <input type="checkbox" name="check"></input> 
-                <label htmlFor="box-shadow"> Viktig? </label>
-            </div>
+    function onChangeVurdering(event) {
+        setVurdering(event.target.value);
+    }
 
-            </div>
-            
+    function onChangeNerhet(event) {
+        setNerhet(event.target.value);
+    }
+
+    function onChangeCheckedStoy() {
+        setCheckStoy(!checkStoy);
+    }
+
+    function onChangeCheckedVurdering() {
+        setCheckVurdering(!checkVurdering);
+    }
+
+    function onChangeCheckedNerhet() {
+        setCheckNerhet(!checkNerhet);
+    }
+
+    function YagerIntersection() {
+        let liste_med_plasser = data.data;
+        const valgtVurdering = vurdering;
+        const valgtStoy = stoy;
+        const valgtAvstand = nerhet;
+        const vekt = 10;
+        let brukerPosisjon = 0;
+        navigator.geolocation.getCurrentPosition((position) => {
+            brukerPosisjon = position.coords;
+        });
+        for (let i = 0; i < liste_med_plasser.length; i++) {
+            const plass = liste_med_plasser[i];
+            const avstand = 0; // Må bergene avstand her
+            const a = 1 - Math.abs(plass.Vurdering - valgtVurdering) / 5;
+            const b = 1 - Math.abs(plass.StoyNiva - valgtStoy) / 5;
+            let c =
+                avstand < valgtAvstand
+                    ? 1
+                    : Math.abs(avstand - valgtAvstand) / 1000;
+            if (c < 0) {
+                c = 0;
+            }
+            const snitt =
+                1 -
+                Math.min(
+                    1,
+                    ((1 - a) ** vekt + (1 - b) ** vekt + (1 - c) ** vekt) **
+                        (1 / vekt)
+                );
+            console.log("Vurdering: ", a, "Støynivå: ", b, "Snittet: ", snitt);
+        }
+    }
+
+    function middleNumber() {
+        let liste_med_plasser = data.data;
+    }
+
+    function handleSubmit(event) {
+        if (!checkNerhet && !checkStoy && !checkVurdering) {
+            YagerIntersection();
+        }
+        // navigate("/");
+    }
+    return (
+        <>
             <div>
-                <input type="radio" name="styles1" id="box-shadow" className="custom-radio" value={1}></input><label htmlFor="box-shadow">1</label>
-                <input type="radio" name="styles1" id="box-shadow" className="custom-radio" value={2}></input><label htmlFor="box-shadow">2</label>
-                <input type="radio" name="styles1" id="box-shadow" className="custom-radio" value={3}></input><label htmlFor="box-shadow">3</label>
-                <input type="radio" name="styles1" id="box-shadow" className="custom-radio" value={4}></input><label htmlFor="box-shadow">4</label>
-                <input type="radio" name="styles1" id="box-shadow" className="custom-radio" value={5}></input><label htmlFor="box-shadow">5</label>
+                <NavBar></NavBar>
             </div>
-        </div>
-        <div id='element'>
-            <h2>Vurdering</h2>
-            <div>
-                <input type="radio" name="styles2" id="box-shadow" className="custom-radio" value={1}></input><label htmlFor="box-shadow">1</label>
-                <input type="radio" name="styles2" id="box-shadow" className="custom-radio" value={2}></input><label htmlFor="box-shadow">2</label>
-                <input type="radio" name="styles2" id="box-shadow" className="custom-radio" value={3}></input><label htmlFor="box-shadow">3</label>
-                <input type="radio" name="styles2" id="box-shadow" className="custom-radio" value={4}></input><label htmlFor="box-shadow">4</label>
-                <input type="radio" name="styles2" id="box-shadow" className="custom-radio" value={5}></input><label htmlFor="box-shadow">5</label>
+            <header class="header">
+                <h1>Finn studieplass</h1>
+            </header>
+            <p>Her kan du legge inn dine ønsker for studieplassen</p>
+            <div id="filterBox">
+                <div id="element">
+                    <div class="headerWeights">
+                        <div>
+                            <h2>Støy</h2>
+                        </div>
+                        <div id="weight">
+                            <input
+                                type="checkbox"
+                                name="check"
+                                onChange={onChangeCheckedStoy}
+                            ></input>
+                            Viktig?
+                        </div>
+                        <br />
+                        <div onChange={onChangeStoy}>
+                            <input type="radio" id="1" value="1" name="stoy" />{" "}
+                            1
+                            <input
+                                type="radio"
+                                id="1"
+                                value="2"
+                                name="stoy"
+                            />{" "}
+                            2
+                            <input
+                                type="radio"
+                                id="1"
+                                value="3"
+                                name="stoy"
+                            />{" "}
+                            3
+                            <input
+                                type="radio"
+                                id="1"
+                                value="4"
+                                name="stoy"
+                            />{" "}
+                            4
+                            <input
+                                type="radio"
+                                id="1"
+                                value="5"
+                                name="stoy"
+                            />{" "}
+                            5
+                        </div>
+                    </div>
+                </div>
+                <div id="element">
+                    <div class="headerWeights">
+                        <div>
+                            <h2>Vurdering</h2>
+                        </div>
+                        <div id="weight">
+                            <input
+                                type="checkbox"
+                                name="check"
+                                onChange={onChangeCheckedVurdering}
+                            ></input>
+                            Viktig?
+                        </div>
+                        <br />
+                        <div onChange={onChangeVurdering}>
+                            <input
+                                type="radio"
+                                id="1"
+                                value="1"
+                                name="vurdering"
+                            />{" "}
+                            1
+                            <input
+                                type="radio"
+                                id="1"
+                                value="2"
+                                name="vurdering"
+                            />{" "}
+                            2
+                            <input
+                                type="radio"
+                                id="1"
+                                value="3"
+                                name="vurdering"
+                            />{" "}
+                            3
+                            <input
+                                type="radio"
+                                id="1"
+                                value="4"
+                                name="vurdering"
+                            />{" "}
+                            4
+                            <input
+                                type="radio"
+                                id="1"
+                                value="5"
+                                name="vurdering"
+                            />{" "}
+                            5
+                        </div>
+                    </div>
+                </div>
+                <div id="element">
+                    <div class="headerWeights">
+                        <div>
+                            <h2>Avstand</h2>
+                        </div>
+                        <div id="weight">
+                            <input
+                                type="checkbox"
+                                name="check"
+                                onChange={onChangeCheckedNerhet}
+                            ></input>
+                            Viktig?
+                        </div>
+                        <br />
+                        <input
+                            className="slidecontainer"
+                            type="range"
+                            step={0.1}
+                            min={1}
+                            max={5}
+                            value={nerhet}
+                            onChange={onChangeNerhet}
+                        />
+                        <p>km: {nerhet}</p>
+                    </div>
+                </div>
+
+                {/*
+            <div id='element'>
+                <div class='headerWeights'>
+                    <div>
+                        <h2>Tilgjengelighet</h2>
+                    </div>
+                <div id= 'weight'>
+                    <input type="checkbox" name="check"></input>
+                    <label htmlFor="box-shadow"> Viktig? </label>
+                </div>
+
+                </div>
+
+                <div>
+                    <input type="radio" name="styles1" id="box-shadow" className="custom-radio" value={1}></input><label htmlFor="box-shadow">1</label>
+                    <input type="radio" name="styles1" id="box-shadow" className="custom-radio" value={2}></input><label htmlFor="box-shadow">2</label>
+                    <input type="radio" name="styles1" id="box-shadow" className="custom-radio" value={3}></input><label htmlFor="box-shadow">3</label>
+                    <input type="radio" name="styles1" id="box-shadow" className="custom-radio" value={4}></input><label htmlFor="box-shadow">4</label>
+                    <input type="radio" name="styles1" id="box-shadow" className="custom-radio" value={5}></input><label htmlFor="box-shadow">5</label>
+                </div>
             </div>
-        </div>
-        <div id='element'>
-            <h2>Nærhet</h2>
-            <div>
-                <input type="radio" name="styles3" id="box-shadow" className="custom-radio" value={1}></input><label htmlFor="box-shadow">1</label>
-                <input type="radio" name="styles3" id="box-shadow" className="custom-radio" value={2}></input><label htmlFor="box-shadow">2</label>
-                <input type="radio" name="styles3" id="box-shadow" className="custom-radio" value={3}></input><label htmlFor="box-shadow">3</label>
-                <input type="radio" name="styles3" id="box-shadow" className="custom-radio" value={4}></input><label htmlFor="box-shadow">4</label>
-                <input type="radio" name="styles3" id="box-shadow" className="custom-radio" value={5}></input><label htmlFor="box-shadow">5</label>
+            <div id='element'>
+                <h2>Vurdering</h2>
+                <div>
+                    <input type="radio" name="styles2" id="box-shadow" className="custom-radio" value={1}></input><label htmlFor="box-shadow">1</label>
+                    <input type="radio" name="styles2" id="box-shadow" className="custom-radio" value={2}></input><label htmlFor="box-shadow">2</label>
+                    <input type="radio" name="styles2" id="box-shadow" className="custom-radio" value={3}></input><label htmlFor="box-shadow">3</label>
+                    <input type="radio" name="styles2" id="box-shadow" className="custom-radio" value={4}></input><label htmlFor="box-shadow">4</label>
+                    <input type="radio" name="styles2" id="box-shadow" className="custom-radio" value={5}></input><label htmlFor="box-shadow">5</label>
+                </div>
             </div>
-        </div>
-        <div id='element'>
-            <button id='sok' onClick={handleSubmit}>Finn leseplass</button>
-        </div>
-    </div>
-    </>
-  );
+            <div id='element'>
+                <h2>Nærhet</h2>
+                <div>
+                    <input type="radio" name="styles3" id="box-shadow" className="custom-radio" value={1}></input><label htmlFor="box-shadow">1</label>
+                    <input type="radio" name="styles3" id="box-shadow" className="custom-radio" value={2}></input><label htmlFor="box-shadow">2</label>
+                    <input type="radio" name="styles3" id="box-shadow" className="custom-radio" value={3}></input><label htmlFor="box-shadow">3</label>
+                    <input type="radio" name="styles3" id="box-shadow" className="custom-radio" value={4}></input><label htmlFor="box-shadow">4</label>
+                    <input type="radio" name="styles3" id="box-shadow" className="custom-radio" value={5}></input><label htmlFor="box-shadow">5</label>
+                </div>
+            </div> */}
+                <div id="element">
+                    <button id="sok" onClick={handleSubmit}>
+                        Finn leseplass
+                    </button>
+                </div>
+            </div>
+        </>
+    );
 }
